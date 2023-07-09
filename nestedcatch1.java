5d77b25729467464bd80014c322fd7b697a78c00
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-09 14:08:21 +0530

    Create nestedcatch1.java

---
+++ nestedcatch1.java
@@ -0,0 +1,25 @@
+/* try-catch inside catch block */
+class nestedcatch
+{
+    public static void main(String[] args) {
+
+        try
+        {
+            System.out.println(10/2);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            try
+            {
+                String a="ABHI";
+                System.out.println(a.toLowerCase());
+            }
+            catch(NullPointerException n)
+            {
+                System.out.println("null value can't converted");
+            }
+        }
+        System.out.println("main method ended");  
+    }
+}
